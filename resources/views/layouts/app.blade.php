6889a5e2ae2c674b53c7172cb32e43aee64be85e
RaisSetiadi <rsetiadi2005@gmail.com>
2025-08-08 01:37:57 +0000

    update app blade server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Genharum</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{ asset('image/favicon/favicon.ico') }}">
 

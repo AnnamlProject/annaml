d940e0c232b5b6c2abb8784ba8bc2acac1aec7f9
RaisSetiadi <rsetiadi2005@gmail.com>
2025-10-01 13:49:29 +0700

    perbaikan navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1262,7 +1262,7 @@
                     @endcan
 
                     <!-- Report Dropdown -->
-                    @can('report_menu.access')
+                    @can('fiscal.access')
                         <div class="relative ml-1" x-data="{ open: false }">
                             <button @click="open = !open" @keydown.escape="open = false"
                                 class="flex items-center px-3 py-2 text-gray-700 font-medium hover:text-blue-600 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 rounded-md"
